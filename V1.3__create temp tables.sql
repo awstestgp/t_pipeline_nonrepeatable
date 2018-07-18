5a8c2cb12892611be8764e3d5b369a22c1ba9ad1
awstestgp <41288121+awstestgp@users.noreply.github.com>
2018-07-18 13:21:06 +0530

    Update V1.3__create temp tables.sql

--- V1.3__create temp tables.sql
+++ V1.3__create temp tables.sql
@@ -1,11 +1,11 @@
-Create table emp_demo
+Create table emp_demo1
 (
 emp_id number ,
 emp_name varchar2(10),
 emp_adddr varchar2(100)
 );
 
-Create table emp_per
+Create table emp_per1
 (
 emp_id number,
 emp_name varchar2(10),
@@ -13,7 +13,7 @@ emp_adddr varchar2(100),
 mobile_no number 
 );
 
-Create table emp_add
+Create table emp_add1
 (
 emp_id number ,
 emp_name varchar2(10),
